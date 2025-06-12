1cdee5fdc1aa3c84210e74290909812c550540e8
Helen To <helen.to@Mac.mynet>
2025-06-12 12:57:13 +0100

    handling static tables, jsexecutor, explicitwait

--- src/main/scala/Automation/Week5Day4/HandlingStaticTables.scala
+++ src/main/scala/Automation/Week5Day4/HandlingStaticTables.scala
@@ -1,18 +1,29 @@
 package Automation.Week5Day4
 
-import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-
+import org.openqa.selenium.{By, WebDriver}
+//
 object HandlingStaticTables extends App {
 //INITIATING THE CHROMEDRIVER FOR INTERACTING WITH THE CHROME BROWSER
-  val driver: WebDriver = new ChromeDriver()
-//ACCESSING THE WEB URL FOR TESTING
-  driver.get("")
+val driver: WebDriver = new ChromeDriver
+driver.get("https://www.w3schools.com/html/html_tables.asp")
+driver.manage().window().maximize()
+
+val tableRows = driver.findElements(By.xpath("//*[@id=\"customers\"]/tbody/tr[position() > 1]"))
+
+for (i <- 0  until  tableRows.size()) {
+
+  val cols = tableRows.get(i).findElements(By.tagName("td"))
+  val rowText = cols.toArray().map(_.asInstanceOf[org.openqa.selenium.WebElement].getText)
+
+
+  println(s"Row ${i + 1}: ${rowText.mkString("", " | ", "")}")
 
 
 
+}
 
 
-  driver.quit()
+driver.quit()
 
 }
