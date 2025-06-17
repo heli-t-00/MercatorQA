9bc5b34b864e1f24a3e65c0457ed90b0604caa2f
Helen To <helen.to@Mac.mynet>
2025-06-17 10:03:48 +0100

    start on pm ext task wk6d1

---
+++ src/main/scala/Automation/Week6Day2/TemplateToCopy.scala
@@ -0,0 +1,25 @@
+package Automation.Week6Day2
+
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.{By, WebDriver, WebElement}
+
+object TemplateToCopy extends App {
+//INITIATING THE CHROMEDRIVER FOR INTERACTING WITH THE CHROME BROWSER
+  val driver: WebDriver = new ChromeDriver()
+//ACCESSING THE WEB URL FOR TESTING
+  driver.get("")
+
+
+
+
+
+
+
+
+  driver.quit()
+
+
+
+
+
+}
