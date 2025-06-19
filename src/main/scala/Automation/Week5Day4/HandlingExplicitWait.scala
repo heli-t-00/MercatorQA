20d0594176dbb87a1c325ff8638cb5eb9acd1be1
Helen To <helen.to@Mac.mynet>
2025-06-19 17:26:08 +0100

    update handling explicit wait and jsexecutor with notes

--- src/main/scala/Automation/Week5Day4/HandlingExplicitWait.scala
+++ src/main/scala/Automation/Week5Day4/HandlingExplicitWait.scala
@@ -8,22 +8,38 @@ import java.time.Duration
 
 object ExplicitWait extends App {
 
+  // instantiating the ChromeDriver with Chrome browser
+  
   val driver = new ChromeDriver
+  
+  // access URL for testing
   driver.get("https://www.w3schools.com/howto/howto_css_custom_checkbox.asp")
   driver.manage().window().maximize()
 
-  //explicit wait
+
+    //EXPLICIT WAIT - waits for a certain condition to occur before proceeding further in code
   val explicitWait = new WebDriverWait(driver, Duration.ofMinutes(1))
 
+// waits for this element to be clickable.
   val checkBox = explicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main\"]/div[3]/div[2]/label[1]")))
   println("Checkbox found.")
 
-  val js = driver.asInstanceOf[JavascriptExecutor]
-  js.executeScript("arguments[0].scrollIntoView(true);", checkBox)
-  js.executeScript("arguments[0].click();", checkBox) //button must be clicked in javaScript
-  //  checkBox.click()
+  // ADD JavaScript execution to interact with a web element ie. checkbox when a standard .click() method might not work.
+
+
+  val js = driver.asInstanceOf[JavascriptExecutor] // casts the driver to a JavaScriptExecutor so that you can execute raw JS commands in browser
+  // This is used, because methods like element.click() fails eg. element is off screen/hidden/blocked by other elements. JS can bypass these issues.
+  js.executeScript("arguments[0].scrollIntoView(true);", checkBox) //Scrolls the browser window to bring the checkBox element into view
+  // Elements must be in viewport to interact with them, especially if they are off screen or in a scrollable container
+  js.executeScript("arguments[0].click();", checkBox) //Clicks the checkBox using JS
+  // EXAMPLE(uncomment to see .click() NOT WORK  - comment out the JSExecutor)
+//    checkBox.click()
   println("Checkbox clicked.")
 
   driver.quit()
+
+  // NB: Explicit wait is GOOD for better control compared to implicit waits as it would wait for specific element/conditions to become visible/clickable
+  // Ideal for elements that may take time to load or takes time to become visible /clickable
+  // JSExecutor is used when .click() doesn't work due to element not being clickable/overlay issues/dynamic rendering delays.
 
 }
--- src/main/scala/Automation/Week5Day4/HandlingJSExecutor.scala
+++ src/main/scala/Automation/Week5Day4/HandlingJSExecutor.scala
@@ -9,15 +9,13 @@ object JavaScriptExecutor extends App {
   // Create ChromeDriver instance
   val driver: WebDriver = new ChromeDriver()
 
-//  //SYCHONISATION - implicit wait - apply globally
+//  //SYNCHRONISATION - implicit wait - apply globally
 //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds((10)))
-
-
 
   // Navigate to the website
   driver.get("https://proleed.academy/exercises/selenium/selenium-element-id-locators-practice-form.php")
 
-  //  driver.manage().window().maximize()
+    driver.manage().window().maximize()
 
   val emailAddress: WebElement = driver.findElement(By.id("email")) //find the element by id (find through inspecting webpage
   emailAddress.sendKeys("test@gmail.com") //this is allowing us to enter an input (text) to a field (the email box in this case)
@@ -32,10 +30,16 @@ object JavaScriptExecutor extends App {
   val login: WebElement = driver.findElement(By.id("login")) //find submit button through id
   //  login.click() //click the button
 
+  // Casts the WebDriver instance to JavascriptExecutor so you can execute custom JavaScript in the browser.
+  // This is necessary because not all WebDriver instances directly expose JavaScript execution.
   val js = driver.asInstanceOf[JavascriptExecutor]
+  // Scrolls the `login` element into the visible area of the browser window using JavaScript.
+  // This helps ensure the button is visible before interacting with it (prevents ElementNotInteractableException).
   js.executeScript("arguments[0].scrollIntoView(true);", login)
+  // Clicks on the `login` element using JavaScript instead of the usual WebDriver `.click()` method.
+  // Useful when the normal `.click()` fails due to overlays, hidden elements, or other UI issues.
   js.executeScript("arguments[0].click();", login)
-  println("Login successful")
+  println("Clicked using JS Executor - Login successful")
 
 
   // USING ASSERTION FOR VALIDATION
